cb6bade4ab6bd11acce8b021182dbc36d0cfa227
VitorCioletti <ciolettiv@gmail.com>
2021-04-12 09:01:35 -0300

    Adicionado carregamento de arquivo de configuracao.

--- Servidor/Configuracao/ArquivoConfiguracao.cs
+++ Servidor/Configuracao/ArquivoConfiguracao.cs
@@ -1,4 +1,19 @@
 namespace Servidor.Configuracao
 {
-    
+    using System.IO;
+
+    using Microsoft.Extensions.Configuration;
+
+    public static class ArquivoConfiguracao
+    {
+        public static IConfigurationRoot Configuracoes { get; set; }
+
+        public static void Carrega()
+        {
+            Configuracoes = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"configuracao.json")
+                .Build();
+        }
+    }
 }
